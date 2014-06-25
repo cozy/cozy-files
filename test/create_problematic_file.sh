8d29039521cbe1bb2c781541184f1f9e984e5ee3
Alexis Gavoty <kload@kload.fr>
2014-06-25 09:39:12 +0200

    [fix] Make script compatible with OSX

--- test/create_problematic_file.sh
+++ test/create_problematic_file.sh
@@ -4,6 +4,6 @@
 file_name=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
 
 # Populate file with dd
-dd if=/dev/urandom bs=1M count=2000 of=./$file_name > /dev/null 2>&1
+dd if=/dev/zero bs=1 count=0 seek=2000000000 of=./$file_name > /dev/null 2>&1
 
 echo "File $file_name created"
